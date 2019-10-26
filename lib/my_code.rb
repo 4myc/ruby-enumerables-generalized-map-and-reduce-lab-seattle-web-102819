8e680d370220111a54942259fdf698903085e57d
Amy C <missch3n@gmail.com>
2019-10-26 22:44:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,15 @@ def map(array)
   new_array = []
   counter = 0
   while counter < array.length do
-    new_array([counter += 1]){|n| n * n}
+    new_array.push(yield(array[counter]))
+    counter += 1
   end
   new_array
 end
 
+def reduce(array, starting point=0)
+  total = []
+  counter = 0
+  while counter < array.length do
+    if (total = 
+end
