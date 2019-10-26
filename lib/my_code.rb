f6f4d52936aafc82bd9c853d27223855b9e0b382
Amy C <missch3n@gmail.com>
2019-10-26 22:52:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,6 +14,7 @@ def reduce(array, starting point=0)
   counter = 0
   while counter < array.length do
     total_array = (yield(array[counter]), sum))
+    if !sum = 
     end
     counter += 1
   end
