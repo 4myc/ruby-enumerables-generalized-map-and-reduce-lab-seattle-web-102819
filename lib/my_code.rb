0b4d3c392effab8c306cda773f6491be4922b09d
Amy C <missch3n@gmail.com>
2019-10-26 22:59:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,6 +13,9 @@ def reduce(array, starting_point=0)
   total = starting_point
   counter = 0
   while counter < array.length do
+    if !total
+      return false
+    else
     total = (yield(array[counter], total))
     counter += 1
   end
