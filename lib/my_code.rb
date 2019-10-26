4577a57a06e77a8caa1f4b62dff05d5d8ef0fd4b
Amy C <missch3n@gmail.com>
2019-10-26 22:44:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(array)
   new_array = []
   counter = 0
   while counter < array.length do
-    new_array([counter]){|n| n * n}
+    new_array([counter += 1]){|n| n * n}
   end
   new_array
 end
